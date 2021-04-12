ef6126ca6d3e59e807f91c88b6ebe2651bc52d9b
unknown <eikmei88@gmail.com>
2021-04-12 14:31:34 +0800

    Building React app

--- src/Routes.js
+++ src/Routes.js
@@ -2,17 +2,39 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home/Home";
 import NotFound from "./containers/NotFound/NotFound";
+import Login from "./containers/Login/Login";
+import Signup from "./containers/Signup/Signup";
+import NewNote from "./containers/NewNote/NewNote";
+import Notes from "./containers/Notes/Notes";
+import Settings from "./containers/Settings/Settings";
+import AuthenticatedRoute from "./components/AuthenticatedRoute/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute/UnauthenticatedRoute";
 
 export default function Routes() {
-  return (
-    <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        {/* Finally, catch all unmatched routes */}
-        <Route>
-            <NotFound />
-        </Route>
-    </Switch>
-  );
+	return (
+		<Switch>
+			<Route exact path="/">
+				<Home />
+			</Route>
+			<UnauthenticatedRoute exact path="/login">
+				<Login />
+			</UnauthenticatedRoute>
+			<UnauthenticatedRoute exact path="/signup">
+				<Signup />
+			</UnauthenticatedRoute>
+			<AuthenticatedRoute exact path="/notes/new">
+				<NewNote />
+			</AuthenticatedRoute>
+			<AuthenticatedRoute exact path="/notes/:id">
+				<Notes />
+			</AuthenticatedRoute>
+			<AuthenticatedRoute exact path="/settings">
+				<Settings />
+			</AuthenticatedRoute>
+			{/* Finally, catch all unmatched routes */}
+			<Route>
+				<NotFound />
+			</Route>
+		</Switch>
+	);
 }
--- src/config.js
+++ src/config.js
@@ -1,18 +1,21 @@
 const config = {
-  s3: {
-    REGION: "us-east-1",
-    BUCKET: "file-upload-notes-app",
-  },
-  apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://px8x2yeq79.execute-api.us-east-1.amazonaws.com/prod",
-  },
-  cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_ASW9vvyjZ",
-    APP_CLIENT_ID: "170p6khmgd6evn4ft2o0paltaa",
-    IDENTITY_POOL_ID: "us-east-1:4a7bd94e-3cb8-417e-9621-4f61142a59a2",
-  },
+	s3: {
+		REGION: "us-east-1",
+		BUCKET: "file-upload-notes-app",
+	},
+	apiGateway: {
+		REGION: "us-east-1",
+		URL: "https://px8x2yeq79.execute-api.us-east-1.amazonaws.com/prod",
+	},
+	cognito: {
+		REGION: "us-east-1",
+		USER_POOL_ID: "us-east-1_ASW9vvyjZ",
+		APP_CLIENT_ID: "170p6khmgd6evn4ft2o0paltaa",
+		IDENTITY_POOL_ID: "us-east-1:4a7bd94e-3cb8-417e-9621-4f61142a59a2",
+	},
+	MAX_ATTACHMENT_SIZE: 5000000,
+	STRIPE_KEY:
+		"pk_test_51IehmtFYrYnTv4T12tASNnPKh6h4UxLhRfNGlZDxnStwEKuScTPAWMXclyVrACkMS8L3KZNmE9w28afjUjYYyxf3002DtzGy71",
 };
 
 export default config;
